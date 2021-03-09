58946dd41e26f57e4c12536e891ba46ca785e661
Yabkoy <yabuszko05@gmail.com>
2021-03-09 23:22:42 +0100

    add comments

--- AimKulkiSFML/src/Game.cpp
+++ AimKulkiSFML/src/Game.cpp
@@ -2,7 +2,7 @@
 
 Game::Game()
 {
-	RW = new sf::RenderWindow(sf::VideoMode(WIN_W, WIN_H), "Title"); //Creating Window and allocating SFML RenderWindow
+	RW = new sf::RenderWindow(sf::VideoMode(WIN_W, WIN_H), "AddectiveGame"); //Creating Window and allocating SFML RenderWindow
 	mainShapesPointer.reserve(2);
 	mainShapesPointer.emplace_back(&yourCharacter);
 	mainShapesPointer.emplace_back(&enemyCharacter);
@@ -28,13 +28,14 @@ void Game::initObjects()
 		castObject->setFillColor(sf::Color::Red);
 		castObject->setPosition(sf::Vector2f(WIN_W / 2, WIN_H / 2));
 	}
+
 }
 
 void Game::update()
 {
 	while (RW->isOpen()) //Main Game loop
 	{
-		if (RW->pollEvent(mainEvent))
+		if (RW->pollEvent(mainEvent)) // Poll Event
 		{
 			if (mainEvent.type == sf::Event::Closed | sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
 				RW->close();
@@ -42,18 +43,16 @@ void Game::update()
 		RW->clear(sf::Color::Black); //Clearing background to Black
 
 
-		if (yourCharacter.getGlobalBounds().contains(sf::Mouse::getPosition(*RW).x, sf::Mouse::getPosition(*RW).y) && sf::Mouse::isButtonPressed(sf::Mouse::Left))
-			yourCharacter.setPosition(sf::Vector2f(sf::Mouse::getPosition(*RW).x, sf::Mouse::getPosition(*RW).y));
+		if (yourCharacter.getGlobalBounds().contains(sf::Mouse::getPosition(*RW).x, sf::Mouse::getPosition(*RW).y) && sf::Mouse::isButtonPressed(sf::Mouse::Left)) //On Mouse pressed - obejct move
+			yourCharacter.setPosition(sf::Vector2f(sf::Mouse::getPosition(*RW).x, sf::Mouse::getPosition(*RW).y)); 
 
-		if (yourCharacter.getGlobalBounds().intersects(enemyCharacter.getGlobalBounds()))
+		if (yourCharacter.getGlobalBounds().intersects(enemyCharacter.getGlobalBounds())) //Enemy collision detection
 		{
 			yourCharacter.setFillColor(sf::Color(rand() % 255, rand() % 255, rand() % 255));
 			enemyCharacter.setPosition(sf::Vector2f(rand() % WIN_W, rand() % WIN_H));
 			enemyCharacter.setFillColor(sf::Color(rand() % 255, rand() % 255, rand() % 255));
 		}
 			
-
-
 		for (sf::Shape* curretObejct : mainShapesPointer) //drawing object loop
 		{
 			RW->draw(*curretObejct);
